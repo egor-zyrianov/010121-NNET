0448a2e6707a831a08ed47edef2831f0763d5846
Йэориос <egor.zyrianov@gmail.com>
2021-04-27 23:13:05 +0700

    27.04.2021
    Additional dimension

--- 001.py
+++ 001.py
@@ -1,45 +1,46 @@
 # NNET
 # 270421
 import numpy as np
-import os
 ep = 1e-7
 
 def d(f):
-    def D(X):
-        dfx = X.copy()
-        for j, xj in enumerate(X[0]):
-            temp = X.copy()
-            temp[:,j] += ep
-            dfx[:,j]  = f(temp) - f(X)
-            dfx[:,j] /= ep
-            del temp
-        return(dfx)
-    return(D)
+def D(W):
+dfw = W.copy()
+for j, wj in enumerate(W[0]):
+temp = W.copy()
+temp[:,j] += ep
+dfw[:,j] = f(temp) - f(W)
+dfw[:,j] /= ep
+del temp
+return(dfw)
+return(D)
 
 class brickwall():
-    def __init__(self, X, nodes):
-        self.X = np.array(X)
-        self.W = np.random.rand(nodes, len(X))
-        self.Y = self.Ff(self.W)
-    def Ff(self,W):
-        result = np.array(W)*self.X
-        result = np.array([sum(fi) for fi in result])
-        return(result)
-    def Qf(self, A, gain = 1,):
-        self.A = A
-        Q = lambda W: (self.Ff(W) - A)**2
-        self.D = d(Q)(self.W)
-        self.D += ep
-        for count in range(gain):
-            self.W -= Q(self.W)/self.D/len(X)
-        self.Y = self.Ff(self.W)
-        print(Q(self.W))
+def __init__(self, X, nodes):
+self.X = np.array(X)
+self.W = np.random.rand(nodes, len(X))
+self.Y = self.Ff(self.W)
+def Ff(self,W):
+result = np.array(W)*self.X
+result = np.array([sum(fi) for fi in result])
+return(result)
+def Qf(self, A, gain = 1,):
+self.A = np.array([A])
+Q = lambda W: ([self.Ff(W)] - self.A)**2
+self.D = d(Q)(self.W)
+self.D += ep
+for count in range(gain):
+print(Q(self.W))
+temp = np.transpose(Q(self.W))
+self.W -= temp/self.D/len(X)
+self.Y = self.Ff(self.W)
+print(Q(self.W))
 
-X = [7, 0, 0]
-A = 1
+X = [7, 1, 2]
+A = [1,0]
 X = np.array(X)
-l = brickwall(X, 1)
+l = brickwall(X, 2)
 for i in range(10):
-    l.Qf(A)
+l.Qf(A)
 
 print(l.Y)
