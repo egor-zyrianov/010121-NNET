4ba8f5a85cbbf3e1d970220b2e08886847a5336a
Йэориос <egor.zyrianov@gmail.com>
2021-04-30 23:19:22 +0700

    30.04.2021
    runcell(0, 'C:/Users/B150M-C/.spyder-py3/temp.py')
    C:\Users\B150M-C\.spyder-py3\temp.py:31: VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated. If you meant to do this, you must specify 'dtype=object' when creating the ndarray
      self.A = np.array([X,A])
    Traceback (most recent call last):
    
      File "C:\Users\B150M-C\.spyder-py3\temp.py", line 52, in <module>
        Wl1.Qf(X, A1)
    
      File "C:\Users\B150M-C\.spyder-py3\temp.py", line 32, in Qf
        self.A = [self.A[:,i] for i,x in enumerate(X)]
    
      File "C:\Users\B150M-C\.spyder-py3\temp.py", line 32, in <listcomp>
        self.A = [self.A[:,i] for i,x in enumerate(X)]
    
    IndexError: too many indices for array: array is 1-dimensional, but 2 were indexed

--- 001.py
+++ 001.py
@@ -1,46 +1,54 @@
 # NNET
-# 270421
+# 300421
+# 1.стыковка слоев
+# 2.апроксимация на множественной выборке
+# 3.автоматическое дифференцирование
 import numpy as np
 ep = 1e-7
 
 def d(f):
-def D(W):
-dfw = W.copy()
-for j, wj in enumerate(W[0]):
-temp = W.copy()
-temp[:,j] += ep
-dfw[:,j] = f(temp) - f(W)
-dfw[:,j] /= ep
-del temp
-return(dfw)
-return(D)
-
+    def D(W):
+        dfw = W.copy()
+        for j, wj in enumerate(W[0]):
+            temp = W.copy()
+            temp[:,j] += ep
+            dfw[:,j] = f(temp) - f(W)
+            dfw[:,j] /= ep
+            del temp
+            return(dfw)
+        return(D)
+    
 class brickwall():
-def __init__(self, X, nodes):
-self.X = np.array(X)
-self.W = np.random.rand(nodes, len(X))
-self.Y = self.Ff(self.W)
-def Ff(self,W):
-result = np.array(W)*self.X
-result = np.array([sum(fi) for fi in result])
-return(result)
-def Qf(self, A, gain = 1,):
-self.A = np.array([A])
-Q = lambda W: ([self.Ff(W)] - self.A)**2
-self.D = d(Q)(self.W)
-self.D += ep
-for count in range(gain):
-print(Q(self.W))
-temp = np.transpose(Q(self.W))
-self.W -= temp/self.D/len(X)
-self.Y = self.Ff(self.W)
-print(Q(self.W))
+    def __init__(self, X, nodes):
+        self.pipe = len(X[0])
+        self.W = np.random.rand(nodes, self.pipe)
+    def Ff(self, X, W=None):
+        if (W==None).all(): W = self.W
+        result = W*X
+        result = np.array([sum(fi) for fi in result])
+        return(result)
+    def Qf(self, X, A, gain = 1,):
+        self.A = np.array([X,A])
+        self.A = [self.A[:,i] for i,x in enumerate(X)]
+        temp = lambda x,w,a: ([self.Ff(w,x)] - a)**2
+        Q = lambda w: sum([temp(x,w,a) for (x,a) in self.A])
+        self.D = d(Q)(self.W)
+        self.D += ep
+        for count in range(gain):
+            temp = np.transpose(Q(self.W))
+            print(temp)
+            self.W -= temp/self.D/self.pipe
+        print(Q(self.W))
 
-X = [7, 1, 2]
-A = [1,0]
-X = np.array(X)
-l = brickwall(X, 2)
+X1 = [7, 1, 2]
+X2 = [1, 5, 3]
+X = np.array([X1, X2])
+A1 = [1]
+A2 = [0]
+A = np.array([A1, A2])
+Wl1 = brickwall(X, 1)
+#Wl2 = brickwall(Wl1.Y, 1)
 for i in range(10):
-l.Qf(A)
+    Wl1.Qf(X, A1)
 
-print(l.Y)
+print(Wl1.Y)
